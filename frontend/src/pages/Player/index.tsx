b1d4934bed14ecdbacfb33f000c23ad280d1c3bb
kyndv <kyndv@hasaki.vn>
2023-11-29 18:21:26 +0700

    add image

--- frontend/src/pages/Player/index.tsx
+++ frontend/src/pages/Player/index.tsx
@@ -7,6 +7,8 @@ import { Header } from '../../components/Header';
 
 export const Player = () => {
 	const [audioLists, setAudioLists] = useState<AudioList>();
+	const [loading, setLoading] = useState<boolean>(false);
+	const [queue, setQueue] = useState<Array<any>>([]);
 
 	return (
 		<>
@@ -19,6 +21,118 @@ export const Player = () => {
 				// setMoreOptionsOpened={setMoreOptionsOpened}
 				// setArtist={setArtist}
 				/>
+				<div className="playlistsToAdd">
+					{/* {artist && artist.photo !== '' && playlistsToAdd && playlistsToAdd.length > 0 && !loading && (
+						<div className="artist" onClick={() => handleSearchArtist(artist.id)}>
+							<div className="background" style={{ backgroundImage: `url('${artist.photo}')` }}></div>
+						</div>
+					)} */}
+
+					{/* {playlistsToAdd &&
+						playlistsToAdd.length > 0 &&
+						!loading &&
+						playlistsToAdd.map((i, k) => (
+							// @ts-ignore
+							<Playlist
+								title={i.title}
+								id={i.id}
+								songs={i.songs}
+								thumb={i.thumb}
+								setPlaylistModalOpened={setPlaylistModalOpened}
+								key={k}
+								setCurrentPlaylist={setCurrentPlaylist}
+							/>
+						))} */}
+				</div>
+
+				<div className="items">
+					{/* {videos.length > 0 && !loading ? (
+						videos.map((i, k) => (
+							<Item
+								key={k}
+								thumb={i.thumb}
+								title={i.title}
+								author={i.author}
+								views={i.views}
+								id={i.id}
+								duration={i.duration}
+								setCurrentAudio={setCurrentAudio}
+								setCurrentStats={setCurrentStats}
+								position={0}
+								// @ts-ignore
+								playlist={{}}
+							/>
+						))
+					) : loading ? (
+						<ReactLoading type="spin" color="#999" width={36} className="spinner" />
+					) : (
+						<div className="playlists">
+							<div title="Favorites" className="playlist" onClick={() => setFavoritesModalOpened(true)}>
+								<div className="backgrounds" style={{ backgroundImage: `url('${FavoritesBackground}')` }} />
+							</div>
+
+							<div title="Local" className="playlist" onClick={() => setLocalModalOpened(true)}>
+								<div className="backgrounds" style={{ backgroundImage: `url('${LocalBackground}')` }} />
+							</div>
+
+							{playlists.length > 0 &&
+								playlists.map((i, k) =>
+									i.photo ? (
+										<div title={i.name} key={k} className="artist" onClick={() => handleArtist(k)}>
+											<div className="background" style={{ backgroundImage: `url('${i.photo}')` }}></div>
+										</div>
+									) : (
+										<div title={i.title} key={k} className="playlist" onClick={() => handlePlaylist(k)}>
+											<div className="background" style={{ backgroundImage: `url('${i.thumb}')` }}></div>
+										</div>
+									)
+								)}
+							{videos.length === 0 && !loading && (
+								<div className="playlist" onClick={() => setNewPlaylistModalOpened(true)}>
+									<div className="background"></div>
+
+									<div className="buttons">
+										<img src={AddToQueue} width={28} />
+									</div>
+								</div>
+							)}
+						</div>
+					)} */}
+				</div>
+
+				<div className="queue">
+					{/* {!loading &&
+						videos &&
+						videos.length === 0 &&
+						(queue && queue.length > 0 ? (
+							queue.map((i, k) => (
+								<QueueItem
+									key={k}
+									position={k}
+									title={i.title}
+									thumb={i.thumb}
+									author={i.author}
+									duration={i.duration}
+									id={i.id}
+								/>
+							))
+						) : currentStats && currentStats.title !== '' ? (
+							<QueueItem
+								position={0}
+								title={currentStats.title}
+								author={currentStats.author}
+								thumb={currentStats.thumb}
+								duration={currentStats.duration}
+								id={currentStats.id}
+							/>
+						) : (
+							<div className="empty">
+								<img src={QueueEmpty} />
+
+								<h3>Queue empty, try to search for a song or play a list. :)</h3>
+							</div>
+						))} */}
+				</div>
 				<MusicPlayer audioLists={SampleAudioList} mode={'full'} />
 			</Container>
 		</>
--- frontend/src/pages/Player/styles.tsx
+++ frontend/src/pages/Player/styles.tsx
@@ -4,4 +4,58 @@ export const Container = styled.div`
 	padding-bottom: 5.5rem;
 	display: flex;
 	flex-direction: column;
+
+	.items {
+		width: 98%;
+		display: flex;
+		flex-direction: column;
+		overflow: auto;
+		align-self: center;
+		margin-top: 1.5rem;
+	}
+
+	.playlistsToAdd {
+		margin-top: 1rem;
+	}
+
+	.items {
+		width: 98%;
+		display: flex;
+		flex-direction: column;
+		overflow: auto;
+		align-self: center;
+		margin-top: 1.5rem;
+	}
+
+	.queue {
+		width: 92.5%;
+		display: flex;
+		flex-direction: column;
+		position: relative;
+		align-self: flex-end;
+		margin-right: 1rem;
+		margin-top: -0.5rem;
+
+		#first {
+			background: #111;
+			margin-bottom: 0.75rem;
+			cursor: auto;
+
+			.title p:first-child {
+				font-weight: 600;
+			}
+
+			.thumbnail {
+				width: 96px;
+				height: 96px;
+				background-size: 192px;
+			}
+		}
+	}
+
+	@media (max-width: 1200px) {
+		.queue {
+			width: 90%;
+		}
+	}
 `;
