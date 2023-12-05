7b0fbb328ab5f14c0048b80d61cae9d14fddf723
Vu Ky <vukynpro@gmail.com>
2023-12-06 00:58:08 +0700

    add more menu in side bar

---
+++ frontend/src/components/V2/icons/list-icon.tsx
@@ -0,0 +1,13 @@
+export const ListIcon = () => {
+	return (
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M7 8H21M7 12H21M7 16H21M3 8H3.01M3 12H3.01M3 16H3.01"
+				stroke="#000000"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+};
--- frontend/src/components/V2/sidebar/sidebar.tsx
+++ frontend/src/components/V2/sidebar/sidebar.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
 import { Sidebar } from './sidebar.styles';
 import { CompaniesDropdown } from './sidebar-logo';
 import { HomeIcon } from '../icons/home-icon';
-import { AccountIcon } from '../icons/account-icon';
+import { PlayIcon } from '../icons/play-circle-icon';
+import { PortraitIcon } from '../icons/portrait-icon';
+import { HeartIcon } from '../icons/heart-icon';
+import { ListIcon } from '../icons/list-icon';
+import { ListMusicIcon } from '../icons/list-music-icon';
+import { TrendingUpIcon } from '../icons/trending-up-icon';
+import { SearchV2Icon } from '../icons/search-v2-icon';
 import { SidebarItem } from './sidebar-item';
 import { SidebarMenu } from './sidebar-menu';
 import { useSidebarContext } from '../layout/layout-context';
 import { useLocation } from 'react-router-dom';
+import { CollapseItems } from './collapse-items';
 
 export const SidebarWrapper = () => {
 	const { pathname } = useLocation();
@@ -26,8 +32,16 @@ export const SidebarWrapper = () => {
 				<div className="flex flex-col justify-between h-full">
 					<div className={Sidebar.Body()}>
 						<SidebarItem title="Home" icon={<HomeIcon />} isActive={pathname === '/'} href="/" />
-						<SidebarMenu title="Main Menu">
-							<SidebarItem isActive={pathname === '/accounts'} title="Accounts" icon={<AccountIcon />} href="accounts" />
+						<SidebarMenu title="Main">
+							<SidebarItem isActive={pathname === '/discover'} title="Discover" icon={<PlayIcon />} href="discover" />
+							<SidebarItem isActive={pathname === '/chart'} title="Chart" icon={<TrendingUpIcon />} href="chart" />
+							<SidebarItem isActive={pathname === '/artist'} title="Artist" icon={<PortraitIcon />} href="artist" />
+							<SidebarItem isActive={pathname === '/search'} title="Search" icon={<SearchV2Icon />} href="search" />
+						</SidebarMenu>
+						<SidebarMenu title="Your collection">
+							<SidebarItem isActive={pathname === '/me/tracks'} title="Tracks" icon={<ListIcon />} href="me/tracks" />
+							<CollapseItems icon={<ListMusicIcon />} items={['Add playlist']} title="Playlists" />
+							<SidebarItem isActive={pathname === '/me/likes'} title="Likes" icon={<HeartIcon />} href="me/likes" />
 						</SidebarMenu>
 					</div>
 				</div>
