3b18fb131c23290f535688c5a5b41912f6e4a337
Vu Ky <vukynpro@gmail.com>
2023-12-06 01:07:37 +0700

    add background img

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,22 @@
 import { Layout } from '../components/V2/layout/layout';
-
+import QueueEmptyImg from '../assets/img/bg-1-1.png';
+import { default as MusicPlayer, ReactJkMusicPlayerAudioListProps as AudioList } from 'react-jinke-music-player';
+import { SampleAudioList } from '../types/data';
+import { Container } from './styles';
 const IndexPage = () => {
 	return (
-		<Layout>
-			<h1>Hello World</h1>
-		</Layout>
+		<>
+			<Layout>
+				<Container>
+					<div className="empty">
+						<img src={QueueEmptyImg} />
+
+						<h3>play some music ~hooman~</h3>
+					</div>
+				</Container>
+			</Layout>
+			{/* <MusicPlayer audioLists={SampleAudioList} /> */}
+		</>
 	);
 };
 
