c90d6a4af5f638b760660e2b2bd6a0b806be2916
vuky <kyndv@hasaki.vn>
2023-12-11 20:55:55 +0700

    add playlist page to route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import IndexPage from './pages';
 import './styles/global.css';
+import PlaylistPage from './components/V2/pages/playlist';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <IndexPage />,
+	},
+	{
+		path: 'discover',
+		element: <PlaylistPage />,
 	},
 ]);
 
