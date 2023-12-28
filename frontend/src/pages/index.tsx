c5bcd6a8a4db4cdc751ae8451ca0641f6f4686ab
Vu Ky <vukynpro@gmail.com>
2023-12-28 22:37:23 +0700

    update home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { memo } from 'react';
-import QueueEmptyImg from '../assets/img/bg-1-1.png';
-import 'react-jinke-music-player/assets/index.css';
+import { memo } from "react";
+import QueueEmptyImg from "../assets/img/bg-1-1.png";
+import "react-jinke-music-player/assets/index.css";
 
 const IndexPage = () => {
 	return (
 		<div className="flex flex-col gap-2">
-			<img src={QueueEmptyImg} />
-			<h3>play some music ~hooman~</h3>
+			<img className="rounded-lg" title="meowws" aria-label="meowws" src={QueueEmptyImg} />
+			<p className="text-xl font-mono font-bold text-center">play some music ~hooman~</p>
 		</div>
 	);
 };
