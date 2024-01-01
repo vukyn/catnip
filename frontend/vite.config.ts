1fc6fe981ba4aa48851e54ed71a517b1efa8255c
linh <tomandjery4444@gmail.com>
2024-01-01 17:28:41 +0700

    feat: find songs in search bar

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	define: {
-		// global: {},
-	},
-	plugins: [react()],
+    define: {
+        // global: {},
+    },
+    plugins: [react()],
+    resolve: {
+        alias: {
+            src: "/src",
+            types: "/src/types",
+        },
+    },
 });
